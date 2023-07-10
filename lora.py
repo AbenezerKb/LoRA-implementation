6dcf6ded12f460a817731fc72fb27f33725337ff
AbenezerKb <aben.jacob3@gmail.com>
2023-07-10 15:26:32 +0300

    add LoRA implementation

--- lora.py
+++ lora.py
@@ -7,7 +7,8 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 from tqdm import tqdm
 
-_  = torch.manual_seed(0)
+
+_ = torch.manual_seed(0)
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.1307,), (0.3081,))])
@@ -24,21 +25,21 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class RichBoyNet(nn.Module):
-    
+
     def __init__(self, hidden_size_1=1000, hidden_size_2=2000):
         super(RichBoyNet, self).__init__()
         self.linear1 = nn.Linear(28*28, hidden_size_1, hidden_size_2)
         self.linear2 = nn.Linear(hidden_size_1, hidden_size_2)
         self.linear3 = nn.Linear(hidden_size_2, 10)
         self.relu = nn.ReLU()
-    
+
     def forward(self, img):
         x = img.view(-1, 28*28)
         x = self.relu(self.linear1(x))
         x = self.relu(self.linear2(x))
         x = self.linear3(x)
         return x
-    
+
 
 net = RichBoyNet().to(device)
 
